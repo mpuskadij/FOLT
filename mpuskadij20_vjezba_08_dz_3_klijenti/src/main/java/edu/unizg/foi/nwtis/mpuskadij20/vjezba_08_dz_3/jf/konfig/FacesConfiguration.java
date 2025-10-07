cc7ac7c7a49c1e2111b92484707bc717a8c5fcee
Marin Puškadija <100581651+mpuskadij@users.noreply.github.com>
2025-10-07 15:34:16 +0200

    Delete mpuskadij20_vjezba_08_dz_3_klijenti directory

--- mpuskadij20_vjezba_08_dz_3_klijenti/src/main/java/edu/unizg/foi/nwtis/mpuskadij20/vjezba_08_dz_3/jf/konfig/FacesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package edu.unizg.foi.nwtis.mpuskadij20.vjezba_08_dz_3.jf.konfig;
-
-import jakarta.faces.annotation.FacesConfig;
-/**
- * Klasa za konfiguraciju Jakarta Faces
- */
-@FacesConfig
-public class FacesConfiguration {
-}
